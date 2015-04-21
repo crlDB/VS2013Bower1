8b282bcb56e7a6c49701b2863bfb8f5345689c4a
crlDB <carl.debels@telenet.be>
2015-04-21 20:16:12 +0200

    initial commit

---
+++ VS2013Bower1/VS2013Bower1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VS2013Bower1.Startup))]
+namespace VS2013Bower1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
